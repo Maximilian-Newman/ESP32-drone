54eb175539d4e534f97ade9bacf995ee77436670
Maximilian Newman Loussouarn <maximilian.newman@icloud.com>
2026-03-13 21:55:56 -0400

    Update and rename drone_code.ino to drone_code.ino
    changing to using PID

--- drone_code/drone_code.ino
+++ drone_code/drone_code.ino
@@ -17,11 +17,13 @@ byte pinC = 3;
 byte pinD = 4;
 
 byte mode = 0;
-byte modeZ = 0;
+//byte modeZ = 0;
 
 const float MAX_ANGULAR_VELOCITY = 5;
 const float MAX_ANGLE = 20;
 const float MAX_SPEED = 1; // arbitrary change later
+//float GYRO_GAIN = 0.0000001;
+float P = 0.000001;
 
 float targetAZ = 0;
 float targetVZ = 0;
@@ -33,6 +35,9 @@ float targetGyroY = 0;
 
 float gyroOffsetX = 0;
 float gyroOffsetY = 0;
+float accOffsetX = 0;
+float accOffsetY = 0;
+float accOffsetZ = 0;
 
 float vz = 0;
 float vx = 0;
@@ -53,8 +58,10 @@ unsigned long lastTime = 0;
 void setup() {
   Serial.begin(115200);
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   digitalWrite(7, HIGH); // LED blue
+  digitalWrite(8, LOW);
   digitalWrite(9, LOW);
 	/*ESP32PWM::allocateTimer(0);
 	ESP32PWM::allocateTimer(1);
@@ -74,6 +81,8 @@ void setup() {
   Wire.begin(11,10);
   if (!mpu.begin(0x68)) {
     Serial.println("Failed to find MPU6050 chip");
+    digitalWrite(7, LOW); // LED red
+    digitalWrite(8, HIGH);
     while (1) {delay(10);}
   }
   //mpu.calcOffsets(true, true);
@@ -90,10 +99,16 @@ void setup() {
     mpu.getEvent(&a, &g, &temp);
     gyroOffsetX += g.gyro.x;
     gyroOffsetY += g.gyro.y;
+    accOffsetX += a.acceleration.x;
+    accOffsetY += a.acceleration.y;
+    accOffsetZ += a.acceleration.z;
     delay(2);
   }
   gyroOffsetX /= numCalibReadings;
   gyroOffsetY /= numCalibReadings;
+  accOffsetX /= numCalibReadings;
+  accOffsetY /= numCalibReadings;
+  accOffsetZ /= numCalibReadings;
 
   WiFi.softAPConfig(apIP, apIP, netMsk);
   WiFi.softAP("AeroHacks Drone 1", "skibidi123");
@@ -101,6 +116,7 @@ void setup() {
 
   Serial.println("ready");
   digitalWrite(7, LOW); // LED green
+  digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
 
   lastTime = millis();
@@ -126,9 +142,9 @@ void loop() {
   float gyroVY = g.gyro.y - gyroOffsetY;
   gyroX -= gyroVX * dt;
   gyroY -= gyroVY * dt;
-  float rawAccZ = a.acceleration.z;
-  float rawAccX = a.acceleration.x;
-  float rawAccY = a.acceleration.y;
+  float rawAccZ = a.acceleration.z - accOffsetZ;
+  float rawAccX = a.acceleration.x - accOffsetX;
+  float rawAccY = a.acceleration.y - accOffsetY;
   float accZ = rawAccZ * cos(gyroX * PI/180) * cos(gyroY * PI/180) - rawAccX * sin(gyroX * PI/180) - rawAccY * sin(gyroY * PI/180);
   float accX = rawAccX * cos(gyroX * PI/180) + rawAccZ * sin(gyroX * PI/180);
   float accY = rawAccY * cos(gyroY * PI/180) + rawAccZ * sin(gyroY * PI/180);
@@ -153,19 +169,21 @@ void loop() {
     else if (instruct == "gyroX") {client.print(String(gyroVX));}
     else if (instruct == "gyroY") {client.print(String(gyroVY));}
     else if (instruct == "gMode") {client.print(String(mode));}
-    else if (instruct == "gModeZ") {client.print(String(modeZ));}
+    //else if (instruct == "gModeZ") {client.print(String(modeZ));}
+    else if (instruct == "gz") {client.print(String(z));}
+    else if (instruct == "gvz") {client.print(String(vz));}
     
     else if (instruct.startsWith("mode")) {
       instruct.remove(0, 4);
       mode = instruct.toInt();
       Serial.print("New Mode: ");
-      Serial.println(mode);
+      Serial.print(mode);
     }
     
-    else if (instruct.startsWith("zmode")) {
-      instruct.remove(0, 4);
-      modeZ = instruct.toInt();
-    }
+    //else if (instruct.startsWith("zmode")) {
+    //  instruct.remove(0, 4);
+    //  modeZ = instruct.toInt();
+    //}
     
     else if (instruct.startsWith("az")) {
       instruct.remove(0, 2);
@@ -196,6 +214,11 @@ void loop() {
       instruct.remove(0, 2);
       targetGyroY = instruct.toInt();
     }
+    
+    else if (instruct.startsWith("gainP")) {
+      instruct.remove(0, 5);
+      P = instruct.toFloat();
+    }
 
     else if (instruct == "manT") {
       thrustA = client.readStringUntil(',').toInt();
@@ -204,103 +227,25 @@ void loop() {
       thrustD = client.readStringUntil('\n').toInt();
     }
 
+    else if (instruct == "incT") {
+      thrustA += client.readStringUntil(',').toInt();
+      thrustB += client.readStringUntil(',').toInt();
+      thrustC += client.readStringUntil(',').toInt();
+      thrustD += client.readStringUntil('\n').toInt();
+    }
+
 
 
 
     client.print("\n");
   }
 
-  if (mode == 1) {modeZ = 1;}
-  if (mode == 0) {modeZ = 0;}
-
-
-  //Serial.println(gyroX);
-
-  if (modeZ >= 4) {
-    if (z < targetZ) {targetVZ += 1 * dt;}
-    else if (z > targetZ) {targetVZ -= 1 * dt;}
+  if (mode == 2){
+    thrustA += P * (gyroX - targetGyroX) * dt;
+    thrustB += P * (gyroX - targetGyroX) * dt;
+    thrustC -= P * (gyroX - targetGyroX) * dt;
+    thrustD -= P * (gyroX - targetGyroX) * dt;
   }
-
-  if (modeZ >= 3) {
-    if (targetVZ > MAX_SPEED) {targetVZ = MAX_SPEED;}
-    if (targetVZ < -MAX_SPEED) {targetVZ = -MAX_SPEED;}
-  }
-
-  if (modeZ >= 2) {
-    if (accZ < targetAZ) {
-      thrustA += 1 * dt;
-      thrustB += 1 * dt;
-      thrustC += 1 * dt;
-      thrustD += 1 * dt;
-    }
-    else if (accZ > targetAZ) {
-      thrustA -= 1 * dt;
-      thrustB -= 1 * dt;
-      thrustC -= 1 * dt;
-      thrustD -= 1 * dt;
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-  if (mode >= 2) {
-    if (gyroVX < targetGyroVX) {
-      thrustA += 1 * dt;
-      thrustB -= 1 * dt;
-      thrustC += 1 * dt;
-      thrustD -= 1 * dt;
-    }
-    else if (gyroVX > targetGyroVX) {
-      thrustA -= 1 * dt;
-      thrustB += 1 * dt;
-      thrustC -= 1 * dt;
-      thrustD += 1 * dt;
-    }
-  }
-
-  if (mode >= 3) {
-    if (gyroX < targetGyroX) {targetGyroVX += 0.1;}
-    else if (gyroX > targetGyroX) {targetGyroVX -= 0.1;}
-
-    if (targetGyroVX > MAX_ANGULAR_VELOCITY) {targetGyroVX = MAX_ANGULAR_VELOCITY;}
-    else if (targetGyroVX < -MAX_ANGULAR_VELOCITY) {targetGyroVX = -MAX_ANGULAR_VELOCITY;}
-  }
-
-  if (targetGyroX > MAX_ANGLE) {targetGyroX = MAX_ANGLE;}
-  else if (targetGyroX < -MAX_ANGLE) {targetGyroX = -MAX_ANGLE;}
-
-
-  if (mode >= 2) {
-    if (gyroVY < targetGyroVY) {
-      thrustA += 1 * dt;
-      thrustB += 1 * dt;
-      thrustC -= 1 * dt;
-      thrustD -= 1 * dt;
-    }
-    else if (gyroVY > targetGyroVY) {
-      thrustA -= 1 * dt;
-      thrustB -= 1 * dt;
-      thrustC += 1 * dt;
-      thrustD += 1 * dt;
-    }
-  }
-
-  if (gyroY < targetGyroY) {targetGyroVY += 0.1;}
-  else if (gyroY > targetGyroY) {targetGyroVY -= 0.1;}
-
-  if (targetGyroVY > MAX_ANGULAR_VELOCITY) {targetGyroVY = MAX_ANGULAR_VELOCITY;}
-  else if (targetGyroVY < -MAX_ANGULAR_VELOCITY) {targetGyroVY = -MAX_ANGULAR_VELOCITY;}
-
-  if (targetGyroY > MAX_ANGLE) {targetGyroY = MAX_ANGLE;}
-  else if (targetGyroY < -MAX_ANGLE) {targetGyroY = -MAX_ANGLE;}
 
 
   if (thrustA < 0) {thrustA = 0;}
